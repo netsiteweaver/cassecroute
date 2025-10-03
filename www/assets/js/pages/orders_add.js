68e6fa5703b24127e0eaa8bc2cd1275275d10daa
SimpleGuy <reeaz@netsiteweaver.com>
2025-10-04 01:32:14 +0400

    increase no of table to 17

--- www/assets/js/pages/orders_add.js
+++ www/assets/js/pages/orders_add.js
@@ -6,8 +6,8 @@ var selectedDestination = null;
 var timeout = null;
 var str = "";
 var x=0;
-var maxTables = 13;
-var tableNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','Terrasse'];
+var maxTables = 18;
+var tableNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','Terrasse'];
 var sHeight = $(window).height();
 
 jQuery(function(){
