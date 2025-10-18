808ea80a51d6c7bfadff8b50b712a356b648bb10
SimpleGuy <reeaz@netsiteweaver.com>
2025-10-19 02:08:49 +0400

    increase no of tables to 25 + Terrasse

--- www/assets/js/pages/orders_add.js
+++ www/assets/js/pages/orders_add.js
@@ -6,8 +6,8 @@ var selectedDestination = null;
 var timeout = null;
 var str = "";
 var x=0;
-var maxTables = 18;
-var tableNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','Terrasse'];
+var maxTables = 26;
+var tableNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','Terrasse'];
 var sHeight = $(window).height();
 
 jQuery(function(){
